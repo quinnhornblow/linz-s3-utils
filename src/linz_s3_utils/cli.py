591097086c6786f3647fb5de555a513d50025eb8
Quinn Hornblow <quinn.hornblow@protonmail.com>
2026-04-09 23:05:06 +1200

    Refactor build_dem_vrts function into cli.py and update script reference in pyproject.toml

---
+++ src/linz_s3_utils/cli.py
@@ -0,0 +1,15 @@
+from linz_s3_utils.vrt import vrt_from_dir
+from linz_s3_utils.constants import S3_ELEVATION_DIR
+
+from pathlib import Path
+
+
+def build_dem_vrts() -> None:
+    vrt_from_dir(
+        S3_ELEVATION_DIR / "new-zealand/new-zealand/dem_1m/2193",
+        Path("data/new-zealand-lidar-1m-dem.vrt"),
+    )
+    vrt_from_dir(
+        S3_ELEVATION_DIR / "new-zealand/new-zealand-contour/dem_8m/2193",
+        Path("data/new-zealand-8m-dem.vrt"),
+    )
