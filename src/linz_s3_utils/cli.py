c80db73039a9de6b39ebcf8fcc297be34f4ac132
Quinn Hornblow <quinn.hornblow@protonmail.com>
2026-04-11 11:45:10 +1200

    rename cli

--- src/linz_s3_utils/cli.py
+++ src/linz_s3_utils/cli.py
@@ -4,7 +4,7 @@ from linz_s3_utils.constants import S3_ELEVATION_DIR
 from pathlib import Path
 
 
-def build_dem_vrts() -> None:
+def build_nz_dem() -> None:
     vrt_from_dir(
         S3_ELEVATION_DIR / "new-zealand/new-zealand/dem_1m/2193",
         Path("data/new-zealand-lidar-1m-dem.vrt"),
