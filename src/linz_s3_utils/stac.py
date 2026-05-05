8f379f85b8697bb19dd5d47c64e24d825d42412a
Quinn Hornblow <quinn.hornblow@protonmail.com>
2026-05-05 15:07:25 +1200

    load

--- src/linz_s3_utils/stac.py
+++ src/linz_s3_utils/stac.py
@@ -10,16 +10,21 @@ import xarray as xr
 from pystac.item import Item
 from pystac_client import Client
 from pystac_client.stac_api_io import StacApiIO
-from pystac_client.warnings import NoConformsTo
+from pystac_client.warnings import FallbackToPystac, NoConformsTo
 
 filterwarnings(
     "ignore",
     category=NoConformsTo,
     module=r"pystac_client\.client",
 )
+filterwarnings(
+    "ignore", category=FallbackToPystac, module=r"pystac_client\.collection_client"
+)
 
 
 class CatalogURLs(Enum):
+    """Enumeration of STAC catalog URLs."""
+
     ELEVATION = "https://nz-elevation.s3-ap-southeast-2.amazonaws.com/catalog.json"
 
 
@@ -36,13 +41,19 @@ class StacCatalogClient:
 
     def __init__(self, catalog: Literal["elevation"] = "elevation"):
         self.client = Client.open(CatalogURLs[catalog.upper()].value, stac_io=stac_io)
-        self.catalog_collections = self.client.get_collections()
 
     def search(
         self,
     ) -> Iterator[Item]:
         """Mimic `pystac_client.Client.search` on a STAC catalog."""
-        return next(self.catalog_collections).get_items()
+        return next(self.client.get_collections()).get_items()
+
+    def list_collections(self) -> list[dict[str, str | None]]:
+        """List ids and titles of collections in the catalog."""
+        return [
+            {"id": collection.id, "title": collection.title}
+            for collection in self.client.get_collections()
+        ]
 
     def load(
         self,
--- tests/linz_s3_utils/test_stac.py
+++ tests/linz_s3_utils/test_stac.py
@@ -1,6 +1,7 @@
 # ruff: noqa: D103
 from collections.abc import Iterator
 
+import pytest
 import xarray as xr
 
 from linz_s3_utils.stac import StacCatalogClient
@@ -10,3 +11,8 @@ def test_stac_catalog_client_instance():
     client = StacCatalogClient()
     assert isinstance(client.search(), Iterator)
     assert isinstance(client.load(), xr.Dataset)
+
+
+def test_stac_invalid_catalog():
+    with pytest.raises(KeyError):
+        StacCatalogClient(catalog="invalid")
